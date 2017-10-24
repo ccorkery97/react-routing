deeb147a5bd28256999e6b9591f8584c620e3c40
Conor Corkery <conorcorkery@Conors-MacBook-Air.local>
2017-10-24 12:29:31 -0500

    reactrouting

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 
 import store from './store';
 
 ReactDOM.render(
+  <BrowserRouter>
   <Provider store={ store }>
     <App />
-  </Provider>, 
+  </Provider>
+  </BrowserRouter>
+, 
   document.getElementById('root')
 );
 registerServiceWorker();
